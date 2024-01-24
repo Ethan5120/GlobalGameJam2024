2550a71d8db9ec8b6dfbbc63e75bf5524e4a02bd
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-01-23 22:57:35 -0800

    InmateSelectionDone

--- Assets/Scripts/CharAsignSystem.cs
+++ Assets/Scripts/CharAsignSystem.cs
@@ -1,13 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CharAsignSystem : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<GameObject> InmateList = new List<GameObject>();
+    public List<GameObject> InnocentList = new List<GameObject>();
     void Start()
     {
-        
+        Debug.Log(InmateList.Count);
+        int test = UnityEngine.Random.Range(0, InmateList.Count - 1);
+        Debug.Log(test);
+        Debug.Log("The impostor is " + InmateList[test].name);
     }
 
     // Update is called once per frame
