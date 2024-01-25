6d308bbb09bc1e5a3200939fdad572365eb2b8cc
ItsSouEzy <113999024+ItsSouEzy@users.noreply.github.com>
2024-01-25 11:15:26 -0600

    audioSOurce

--- Assets/Scripts/Audio/VolumenSettings.cs
+++ Assets/Scripts/Audio/VolumenSettings.cs
@@ -10,4 +10,35 @@ public class VolumenSettings : MonoBehaviour
     [SerializeField] private Slider myMusicSlider;
     [SerializeField] private Slider mySfxSlider;
 
+
+    private void Start()
+    {
+
+        if (PlayerPrefs.HasKey("musicVolumen"))
+        {
+            LoadVolumen();
+        }
+        else 
+        {
+            SetMusicVolumen();
+        }
+
+        
+    }
+
+
+    public void SetMusicVolumen() 
+    {
+        float volumen = myMusicSlider.value;
+        myMixer.SetFloat("Music",Mathf.Log10(volumen)*20);
+        PlayerPrefs.SetFloat("musicVolumen",volumen);
+    }
+
+    private void LoadVolumen() 
+    {
+        myMusicSlider.value = PlayerPrefs.GetFloat("musicVolumen");
+
+        SetMusicVolumen();
+    
+    }
 }
