afd6a3becfa962f18302b59ebc7444835b231aaa
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-01-24 23:05:59 -0800

    SelectionSystemComplete

--- Assets/Scripts/CharAsignSystem.cs
+++ Assets/Scripts/CharAsignSystem.cs
@@ -7,17 +7,61 @@ public class CharAsignSystem : MonoBehaviour
 {
     public List<GameObject> InmateList = new List<GameObject>();
     public List<GameObject> InnocentList = new List<GameObject>();
+    public List<GameObject> SelectedCharacters = new List<GameObject>();
+
+    int impostorId, innocentId;
+    [SerializeField] int innocentAmmount;
     void Start()
     {
-        Debug.Log(InmateList.Count);
-        int test = UnityEngine.Random.Range(0, InmateList.Count - 1);
-        Debug.Log(test);
-        Debug.Log("The impostor is " + InmateList[test].name);
+        SelectedCharacters.Clear();
+        CharacterSetup();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+    void CharacterSetup()
+    {
+        //ImpostorSelection
+        impostorId = UnityEngine.Random.Range(0, InmateList.Count - 1); //Generate a Random number to determine the characte to display
+        Debug.Log(impostorId);
+        Debug.Log("The impostor is " + InmateList[impostorId].name);
+        SelectedCharacters.Add(InmateList[impostorId]); //Add said character to a new list
+
+        //InnocentSelection
+        for(int i = 0; i < innocentAmmount; i++)
+        {
+            innocentId = UnityEngine.Random.Range(0, InnocentList.Count - 1);
+            Debug.Log(innocentId);
+            if(CharCheck())
+            {
+                Debug.Log("The innocent added is " + InnocentList[innocentId].name);
+                SelectedCharacters.Add(InnocentList[innocentId]);
+            }
+            else
+            {
+                Debug.Log(innocentId);
+                Debug.Log("InnocentAlreadySelected, Re-Selecting");
+                i--;
+            }
+            
+            
+        }
+    }
+
+    bool CharCheck()
+    {
+        for(int j = 0; j < SelectedCharacters.Count; j++)
+        {
+            if(InnocentList[innocentId].name == SelectedCharacters[j].name)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
