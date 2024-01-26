2fe238b0b8347d83b4062ef1226e22be6d5017d2
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-01-26 13:14:06 -0600

    Pues ya quedo

--- Assets/Scripts/NewRoundShuffler.cs
+++ Assets/Scripts/NewRoundShuffler.cs
@@ -6,14 +6,23 @@ public class GameRounds
 {
     public string name;
     public List<GameObject> InnmatesInRound = new List<GameObject>();
+    
 }
 
 public class NewRoundShuffler : MonoBehaviour
 {
+    [Header("List of Rounds")]
     public List<GameRounds> Rounds;
+
+    //This is the list of the Random Number Sequence
     private List<int> RNumberSeq = new List<int>();
 
-    [SerializeField] int NoRound;
+    //Declare the GameManager SO; don't forget to add it in the editor after creating one in the assets.
+    [Header("Game Manager")]
+    [SerializeField] ScriptableManager GM;
+    [Space(10)]
+
+    //The positions of the inmates
     Vector2 target;
 
 
@@ -31,25 +40,23 @@ public class NewRoundShuffler : MonoBehaviour
     void RoundSetup()
     {
         //Setup the order
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 4; i++)
         {
-            int r = Random.Range(0, 5);
+            int r = Random.Range(0, 4);
             while (RNumberSeq.Contains(r))
             {
-                r = Random.Range(0, 5);
+                r = Random.Range(0, 4);
             }
             Debug.Log(r);
             RNumberSeq.Add(r);
         }
 
         //Once the shuffle is done, Place then in their positions
-        for(int i = 0; i < 5; i++)
+        for(int i = 0; i < 4; i++)
         {
-            target.x = -6 + (3 * i);   
-            Rounds[NoRound].InnmatesInRound[RNumberSeq[i]].transform.position = target;
+            target.x = -6 + (4 * i);   
+            Rounds[GM.Round].InnmatesInRound[RNumberSeq[i]].transform.position = target;
         }
-
-
 
         
     }
