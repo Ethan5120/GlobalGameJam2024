2eebf23210a3653786443ea34ae3193082c055e3
Fernando Mendoza <fernandogx8647@gmail.com>
2024-01-26 11:15:37 -0600

    explosion

--- Assets/Scripts/InputMouse.cs
+++ Assets/Scripts/InputMouse.cs
@@ -4,12 +4,49 @@ using UnityEngine;
 
 public class InputMouse : MonoBehaviour
 {
-    private void OnMouseEnter() {
-        
+    private string currentState;
+    Animator animator;
+    const string Idle = "idle";
+    const string Stop = "stop";
+    const string Explo = "explosion";
+    bool press = false;
+
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
     }
 
-    private void OnMouseExit() {
-        
+    void ChangeAnimationState(string newState)
+    {
+        if (currentState == newState) return;
+        animator.Play(newState);
+        currentState = newState;
+    }
+
+    private void OnMouseEnter()
+    {
+        if (press == false)
+        {
+            Debug.Log("Entro");
+            ChangeAnimationState(Idle);
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (press == false)
+        {
+            Debug.Log("Salio");
+            ChangeAnimationState(Stop);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        Debug.Log("Presiono el mouse");
+        ChangeAnimationState(Explo);
+        press = true;
     }
 }
 
