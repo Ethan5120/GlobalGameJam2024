8f9be9de79463129f0fcbe77e922c80bb2765f20
Ethan5120 <123921592+Ethan5120@users.noreply.github.com>
2024-01-28 15:30:16 -0800

    Fix

--- Assets/Scenes/Scripts/miguelon/menu.cs
+++ Assets/Scenes/Scripts/miguelon/menu.cs
@@ -19,7 +19,7 @@ public class menu : MonoBehaviour
 
     public void EscenasCambios()
     {
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    SceneManager.LoadScene("GameScene");
 
     }
 
